d0088f158bb997eb2751024e27b06c797f912372
Lev1-athan <LeviGNicholsMail@gmail.com>
2025-01-16 12:59:09 -0500

    Added Next Button

--- ScoutingApp/app/src/main/java/com/example/scoutingapp/MainActivity.java
+++ ScoutingApp/app/src/main/java/com/example/scoutingapp/MainActivity.java
@@ -1,28 +1,15 @@
 package com.example.scoutingapp;
 
-import static java.sql.DriverManager.println;
-
-import static kotlin.text.ScreenFloatValueRegEx.value;
-
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Headers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,39 +18,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.NextPlaceholder), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        Button nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeIntent();
+            }
+
+        });
     }
-
-    public final class AsynchronousGet {
-        private final OkHttpClient client = new OkHttpClient();
-
-        public void run() throws Exception {
-            Request request = new Request.Builder()
-                    .url("https://www.thebluealliance.com/api/v3")
-                    .build();
-
-            client.newCall(request).enqueue(new Callback() {
-                @Override public void onFailure(Call call, IOException e) {
-                    e.printStackTrace();
-                }
-
-                @Override public void onResponse(Call call, Response response) throws IOException {
-                    try (ResponseBody responseBody = response.body()) {
-                        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-
-                        Headers responseHeaders = response.headers();
-                        for (int i = 0, size = responseHeaders.size(); i < size; i++) {
-                            System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
-                        }
-
-                        System.out.println(responseBody.string());
-                    }
-                }
-            });
-        }
+    private void makeIntent()
+    {
+        Intent intent = new Intent(this, AutoActivity.class);
+        startActivity(intent);
+    }
 }
