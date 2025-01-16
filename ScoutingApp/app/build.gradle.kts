e67c5a4912baa5f432dd6b66881fabb9651b0546
26jacquesty <26jacquesty@students.msad54.org>
2025-01-16 12:13:02 -0500

    working on TBA api requests

--- ScoutingApp/app/build.gradle.kts
+++ ScoutingApp/app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 
 dependencies {
 
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- ScoutingApp/app/src/main/java/com/example/scoutingapp/MainActivity.java
+++ ScoutingApp/app/src/main/java/com/example/scoutingapp/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.scoutingapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +29,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+
+    public void getTBARequest{
+        OkHttpClient client = new OkHttpClient();
+
+        Request tbaRequest = new Request.Builder().url("https.thebluealliance.com/api/v3").build();
+
+        Response response = client.newCall(tbaRequest).execute();
+        return response.body().string();
+        response.close();
+
+
+
     }
 }
