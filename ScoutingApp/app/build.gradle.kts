ac378b2b204c7d5ce5cb918b7170b5c4e887c1f7
wildercayden <caydenwb10@gmail.com>
2025-01-16 21:36:01 -0500

    added dependeneys and made sure to keep tylers

--- ScoutingApp/app/build.gradle.kts
+++ ScoutingApp/app/build.gradle.kts
@@ -29,11 +29,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    packaging {
+        resources {
+            excludes += "META-INF/DEPENDENCIES"
+        }
+    }
 }
 
 dependencies {
-
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -41,4 +45,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.apis:google-api-services-sheets:v4-rev581-1.25.0")
+    implementation("com.google.oauth-client:google-oauth-client:1.34.0")
+    implementation("com.google.http-client:google-http-client-gson:1.41.1")
+    implementation("com.google.api-client:google-api-client:1.34.0")
+    implementation("com.google.auth:google-auth-library-oauth2-http:1.14.0")
+    implementation("com.google.auth:google-auth-library-credentials:1.14.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
 }
