4091471ca673f573b2ddeb184f9615b5614bc548
Tylr-J42 <Tyler.Jacques00@gmail.com>
2025-02-04 12:10:29 -0500

    fein

--- ScoutingApp/app/src/main/java/com/example/scoutingapp/MainActivity.java
+++ ScoutingApp/app/src/main/java/com/example/scoutingapp/MainActivity.java
@@ -5,6 +5,8 @@ import static java.sql.DriverManager.println;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,12 +22,15 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import okhttp3.Callback;
 import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    AsynchronousGet getTBAInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +52,20 @@ public class MainActivity extends AppCompatActivity {
         });
 
         TextView TBAView = (TextView)findViewById(R.id.TBATest);
+
+        getTBAInfo = new AsynchronousGet();
+
+        try {
+
+            TBAView.setText(getTBAInfo.getMatchTeams("MELEW", 1)[0][1]);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public final class AsynchronousGet {
         private final OkHttpClient client = new OkHttpClient();
-
-
 
         public String[][] getMatchTeams(String eventKey, int qualMatchNum) throws Exception {
             // https://www.thebluealliance.com/api/v3/event/2024melew/teams?X-TBA-Auth-Key=0zxxGYSvY7xI2onqcWg0NT0sEtmtR6hCpmYJ29nwfxvqrP3Mf1M3lRZO5x6Kc3kt
@@ -61,12 +74,12 @@ public class MainActivity extends AppCompatActivity {
             String fein;
 
             Request request = new Request.Builder()
-                    .url("https://www.thebluealliance.com/api/v3/match/" + eventKey + "_qm" + qualMatchNum + "?X-TBA-Auth-Key=0zxxGYSvY7xI2onqcWg0NT0sEtmtR6hCpmYJ29nwfxvqrP3Mf1M3lRZO5x6Kc3kt")
+                    .url("https://www.thebluealliance.com/api/v3/match/2024melew_qm1?X-TBA-Auth-Key=0zxxGYSvY7xI2onqcWg0NT0sEtmtR6hCpmYJ29nwfxvqrP3Mf1M3lRZO5x6Kc3kt")
                     .build();
 
-            try(Response response = client.newCall(request).execute()){
+            try(Response response = client.newCall(request).enqueue(new Callback())){
                 if(!response.isSuccessful()) throw new IOException("unexpected code " + response);
-
+                Log.d("test", "inside if statement");
                 Headers responseHeaders = response.headers();
                 for(int i = 0; i < responseHeaders.size(); i++){
                     System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
